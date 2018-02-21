722bf983f10179a593658f76006126985cc86142
Oussama Ben Brahim <benbraou@gmail.com>
2018-02-21 22:25:55 +0100

    add more experiments with reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,12 +6,21 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   userForm: FormGroup;
   constructor(public fb: FormBuilder) {
     this.createForm();
+    this.logLastNameChange();
   }
 
+  ngOnInit() {
+    this.userForm.reset();
+    this.userForm.setValue({
+      lastName: 'test lastName',
+      firstName: 'test firstName',
+      profession: 'test profession',
+    });
+  }
   createForm() {
     this.userForm = this.fb.group({
       lastName: ['', Validators.required],
@@ -20,4 +29,9 @@ export class AppComponent {
     });
   }
 
+  logLastNameChange() {
+    const lastNameControl = this.userForm.get('lastName');
+    lastNameControl.valueChanges.forEach(
+      (value: string) => console.log(value));
+  }
 }
