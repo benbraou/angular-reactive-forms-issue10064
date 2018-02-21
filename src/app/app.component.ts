4a9ca5ce4484d3c12c611117423bf66b22c01d08
Oussama Ben Brahim <benbraou@gmail.com>
2018-02-21 23:46:52 +0100

    add sync custom validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -23,7 +23,7 @@ export class AppComponent implements OnInit {
   }
   createForm() {
     this.userForm = this.fb.group({
-      lastName: ['', Validators.required],
+      lastName: ['', [Validators.required, this.validateSyncLastName]],
       firstName: ['', Validators.required],
       profession: ['', Validators.required]
     });
@@ -33,5 +33,14 @@ export class AppComponent implements OnInit {
     const lastNameControl = this.userForm.get('lastName');
     lastNameControl.valueChanges.forEach(
       (value: string) => console.log(value));
+  }
+
+  validateSyncLastName(control: AbstractControl) {
+    if (!control.value || !control.value.includes('custom')) {
+      return {
+        validLastName: true
+      };
+    }
+    return null;
   }
 }
